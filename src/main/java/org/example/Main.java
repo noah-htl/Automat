168d2c72c85a6ee185fce28bb545e180104e8d16
Noah <82201000+Nopro263@users.noreply.github.com>
2024-10-13 15:26:47 +0200

    split

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,27 +1,9 @@
 package org.example;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class Main {
     public static void main(String[] args) {
-        Zustand<String> q1 = new Zustand<>("q1", false);
-        Zustand<String> q2 = new Zustand<>("q2", true);
-        Zustand<String> q3 = new Zustand<>("q3", false);
-        Zustand<String> q4 = new Zustand<>("q4", true);
+        Runnable runnable = new Additum1();
 
-        q1.addUebergang(q2, "1");
-        q2.addUebergang(q3, "0");
-        q2.addUebergang(q4, "1");
-        q3.addUebergang(q3, "0");
-        q4.addUebergang(q4, "1");
-        q4.addUebergang(q3, "0");
-
-        Automat<String> stringAutomat = new Automat<>(q1);
-
-        String[] arr = new String[] {"1","1","0","0"};
-
-        Zustand<String> ergebnis = stringAutomat.run(new ArrayList<>(List.of(arr)));
-        System.out.println(ergebnis);
+        runnable.run();
     }
 }
